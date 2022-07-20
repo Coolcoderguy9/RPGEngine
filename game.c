e7e48407679bf42ca203c4e421582dc5fe868ad1
Coolcoderguy9 <97407877+Coolcoderguy9@users.noreply.github.com>
2022-07-20 16:34:06 -0500

    Update game.c
    moved character naming sequence into a function outside of main, needs revision as current iteration gets stuck in an infinite loop

--- game.c
+++ game.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 // txt based adventure game in c
 
 //character struct and damage formula
+
 
 struct chr {
 	int hp;
@@ -11,10 +13,30 @@ struct chr {
 	int spd;
 };
 
+const char* characterName();
+
 int main() {
-	char name[50];
-	printf("Please enter a name for your character: ");	
-	scanf("%s", name);
-	printf("\nIs %s the name you'd like to use?\n", name);
+	characterName();	
+	printf("Function return success.\n");	
+	
 	return 0;
+}
+
+const char* characterName() {
+	char uin[2];
+	char name[50];
+	while (strcmp(uin, "Y") != 0) {
+		printf("Please enter a name for your character: ");
+		scanf("%", name);
+		if (strlen(name) < 1 || strlen(name) > 50) {
+			printf("Please enter a valid name.\n");
+			continue;
+		}
+		printf("\nIs %s the name you'd like to use? [Y/N] ", name);
+		scanf(" %1c", uin);
+		printf("\n");
+
+	}
+	
+	return name;
 }
