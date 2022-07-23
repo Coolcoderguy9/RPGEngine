955fce80afed6ce12d43f8de2d15f36dbf4f7fb3
Coolcoderguy9 <97407877+Coolcoderguy9@users.noreply.github.com>
2022-07-23 15:04:37 -0500

    Update game.c
    Fixed issue with infinite looping, need to preserve name variable after characterName() function terminates and pass it into main() and subsequently a struct for player character.

--- game.c
+++ game.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 // txt based adventure game in c
 
@@ -18,22 +20,22 @@ const char* characterName();
 int main() {
 	characterName();	
 	printf("Function return success.\n");	
-	
+	printf("%s", name);	
 	return 0;
 }
 
 const char* characterName() {
-	char uin[2];
+	char uin = 0;
 	char name[50];
-	while (strcmp(uin, "Y") != 0) {
+	while (toupper(uin) != 'Y') {
 		printf("Please enter a name for your character: ");
-		scanf("%", name);
+		scanf("%s", name);
 		if (strlen(name) < 1 || strlen(name) > 50) {
 			printf("Please enter a valid name.\n");
 			continue;
 		}
 		printf("\nIs %s the name you'd like to use? [Y/N] ", name);
-		scanf(" %1c", uin);
+		scanf(" %c", &uin);
 		printf("\n");
 
 	}
